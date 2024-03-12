79d30ac62b118c04614326f86b4069eb01012607
VinhTin-AQUA <tinhovinh@gmail.com>
2024-03-12 15:17:34 +0700

    add comment

--- AdoptPet/AdoptPetClient/src/app/services/petfinder.service.ts
+++ AdoptPet/AdoptPetClient/src/app/services/petfinder.service.ts
@@ -5,13 +5,16 @@ import { Injectable } from '@angular/core';
 	providedIn: 'root',
 })
 export class PetfinderService {
-	private accessToken: string = '';
-	private baseUrl = 'https://localhost:7012/api';
+	private baseUrl = 'https://localhost:7012/api'; // url của BE - url sẽ hiển thị trên thanh URL khi chạy BE
 
 	constructor(private http: HttpClient) {}
 
 	getBreeds(type: string) {
+		// tùy chỉnh url của BE
+		// '/Pet/get-breeds?type=' : làm sao biết được => khi test API ở BE, thì url được hiển thị chi tiết trong lời gọi API
 		const url = this.baseUrl + '/Pet/get-breeds?type=' + type;
+
+		// gọi API
 		return this.http.get(url);
 	}
 
@@ -25,14 +28,21 @@ export class PetfinderService {
 		page: number,
 		limit: number
 	) {
+		// tùy chỉnh url của BE
 		const url =
 			this.baseUrl +
 			`/Pet/get-animals?type=${type}&breed=${breed}&age=${age}&size=${size}&gender=${gender}&color=${color}&page=${page}&limit=${limit}`;
+
+		// gọi API
 		return this.http.get(url);
 	}
 
 	getAnimal(id: string) {
+
+		// '/Pet/get-breeds?type=' : làm sao biết được => khi test API ở BE, thì url được hiển thị chi tiết trong lời gọi API
 		const url = this.baseUrl + '/Pet/get-one?id=' + id;
+
+		// gọi API
 		return this.http.get(url);
 	}
 }
