6f156a54cfbdd460ebf27f194e6e8408176b9502
VinhTin-AQUA <tinhovinh@gmail.com>
2024-04-04 22:48:58 +0700

    add: IGenericRepository

---
+++ AdoptPet/AdoptPet.Application/Interfaces/IRepositories/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace AdoptPet.Application.Interfaces.IRepositories
+{
+    public interface IGenericRepository<T>
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<ICollection<T>> GetAllAsync();
+        Task<T> AddAsync(T model);
+        Task<T> UpdateAsync(T model);
+        Task<T> DeleteAsync(T model);
+    }
+}
