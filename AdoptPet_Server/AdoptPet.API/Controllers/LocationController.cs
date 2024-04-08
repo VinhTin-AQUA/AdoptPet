7f65c76fa75c713a1062c5c2e87a8ddf7815ab32
your name <tuongvivttv1106@gmail.com>
2024-04-08 10:54:30 +0700

    added CRUD handling for Location

---
+++ AdoptPet_Server/AdoptPet.API/Controllers/LocationController.cs
@@ -0,0 +1,105 @@
+﻿
+using AdoptPet.Application.DTOs;
+using AdoptPet.Application.DTOs.Breed;
+using AdoptPet.Application.DTOs.Location;
+using AdoptPet.Application.Interfaces.IRepositories;
+using AdoptPet.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdoptPet.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        private readonly IGenericRepository<Location> genericRepository;
+        public LocationController (IGenericRepository<Location> genericRepository)
+        {
+            this.genericRepository = genericRepository;
+        }
+        [HttpGet]
+        [Route("get-location-by-id{id}")]
+        public async Task<IActionResult> GetBreedById(int id)
+        {
+            var r = await genericRepository.GetByIdAsync(id);
+
+            return Ok(new Success<Location> { Status = true, Messages = [], Data = r });
+        }
+
+        [HttpGet]
+        [Route("get-all-location")]
+        public async Task<IActionResult> GetAllLocation()
+        {
+            var locations = await genericRepository.GetAllAsync();
+            return Ok(new Success<List<Location>> { Status = true, Messages = [], Data = locations.ToList() });
+        }
+
+        [HttpPost]
+        [Route("add-location")]
+        public async Task<IActionResult> AddLocation(LocationDto model)
+        {
+            Location newLocation = new Location()
+            {
+                Street = model.Street,
+                Wards = model.Wards,
+                DistrictCity = model.DistrictCity,
+                ProvinceCity = model.ProvinceCity,
+                ZipCode = model.ZipCode
+            };
+
+            var r = await genericRepository.AddAsync(newLocation);
+            return Ok(new Success<Location> { Status = true, Messages = [], Data = r });
+        }
+
+        [HttpPut]
+        [Route("update-location/{id}")]
+        public async Task<IActionResult> UpdateLocation(int id, LocationDto model)
+        {
+            var oldLoction = await genericRepository.GetByIdAsync(id);
+
+            if (oldLoction == null)
+            {
+                return BadRequest(new Success<object> { Status = false, Messages = ["Location not found"], Data = null });
+            }
+            oldLoction.Street = model.Street;
+            oldLoction.Wards = model.Wards;
+            oldLoction.DistrictCity = model.DistrictCity;
+            oldLoction.ProvinceCity = model.ProvinceCity;
+            oldLoction.ZipCode = model.ZipCode;
+
+            /* cap nhat anhr */
+
+            await genericRepository.UpdateAsync(oldLoction);
+
+            return Ok(new Success<LocationDto> { Status = true, Messages = ["Update successfully"], Data = model });
+        }
+
+        [HttpDelete]
+        [Route("delete-permanently-location/{id}")] // 
+        public async Task<IActionResult> DeletePermanentlyLocation(int id)
+        {
+            var location = await genericRepository.GetByIdAsync(id);
+
+            if (location == null)
+            {
+                return BadRequest(new Success<object> { Status = false, Messages = ["Location not found"], Data = null });
+            }
+            await genericRepository.DeletePermanentlyAsync(location);
+            return Ok(new Success<object> { Status = true, Messages = ["Delete successfully"], Data = null });
+        }
+
+        [HttpPut]
+        [Route("soft-delete-location/{id}")] 
+        public async Task<IActionResult> DeleteLoction(int id)
+        {
+            var location = await genericRepository.GetByIdAsync(id);
+
+            if (location == null)
+            {
+                return BadRequest(new Success<object> { Status = false, Messages = ["Location not found"], Data = null });
+            }
+            await genericRepository.SoftDelete(location);
+            return Ok(new Success<object> { Status = true, Messages = ["Delete successfully"], Data = null });
+        }
+    }
+}
---
+++ AdoptPet_Server/AdoptPet.Infrastructure/Repositories/LocationRepository.cs
@@ -0,0 +1,58 @@
+﻿
+
+using AdoptPet.Application.Interfaces.IRepositories;
+using AdoptPet.Domain.Entities;
+using AdoptPet.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdoptPet.Infrastructure.Repositories
+{
+    public class LocationRepository : IGenericRepository<Location>
+    {
+        private readonly AdoptPetDbContext context;
+        public LocationRepository (AdoptPetDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<Location?> AddAsync(Location model)
+        {
+            context.Locations.Add(model);
+            var r = await context.SaveChangesAsync();
+            if (r > 0)
+            {
+                return model;
+            }
+            return null;
+        }
+        
+        public async Task DeletePermanentlyAsync(Location model)
+        {
+            context.Locations.Remove(model);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<ICollection<Location>> GetAllAsync()
+        {
+            var r = await context.Locations.Where(c => c.Status == 0).ToListAsync();
+            return r;
+        }
+
+        public async Task <Location?> GetByIdAsync(int id)
+        {
+            var r = await context.Locations.Where(b => b.Id == id && b.Status == 0).FirstOrDefaultAsync();
+            return r;
+        }
+
+        public async Task SoftDelete(Location model)
+        {
+            model.Status = 1;
+            context.Update(model);
+            await context.SaveChangesAsync();
+        }
+        public async Task UpdateAsync(Location model)
+        {
+            context.Update(model);
+            await context.SaveChangesAsync();
+        }
+    }
+}
