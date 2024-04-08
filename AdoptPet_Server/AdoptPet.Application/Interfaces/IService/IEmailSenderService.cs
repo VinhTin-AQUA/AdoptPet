937a183b2ee08873338a83d4fbec21407eb220ba
phuhuykoxl <phuhuy02003@gmail.com>
2024-04-08 18:09:45 +0700

    Restructor: Create IService folder and put service Interfaces in

---
+++ AdoptPet_Server/AdoptPet.Application/Interfaces/IService/IEmailSenderService.cs
@@ -0,0 +1,10 @@
+﻿using AdoptPet.Domain.Entities;
+
+namespace AdoptPet.Application.Interfaces
+{
+    public interface IEmailSenderService
+    {
+        Task<bool> SendEmailConfirmAsync(AppUser user, string token);
+        Task<bool> SendForgotPasswordEmail(AppUser user, string token);
+    }
+}
