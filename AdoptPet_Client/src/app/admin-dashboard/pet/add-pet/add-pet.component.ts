ab92ab1c2a0bba07789ecb6b495b85c23d1926b5
VinhTin-AQUA <tinhovinh@gmail.com>
2024-04-11 14:07:58 +0700

    update pet gender

--- AdoptPet_Client/src/app/admin-dashboard/pet/add-pet/add-pet.component.ts
+++ AdoptPet_Client/src/app/admin-dashboard/pet/add-pet/add-pet.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-pet',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-pet.component.html',
   styleUrl: './add-pet.component.scss'
 })
 export class AddPetComponent {
+  petForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit() {  
+    this.petForm = this.formBuilder.group({
+      petName: ['', [Validators.required]],
+      email: ['', [Validators.email, Validators.required]],
+      age: ['', [Validators.required]],
+      province: ['',[Validators.required]],
+      district: ['', [Validators.required]],
+      wards: ['', [Validators.required]],
+      street: ['', [Validators.required]],
+      weight: [0, [Validators.required]],
+      petGender: [0],
+      petDesexed: [0],
+      petWormed: [0],
+      petVaccined: [0],
+      petEntryDate: [new Date()],
+    })
+
+    
+  }
 
 }
---
+++ AdoptPet_Server/AdoptPet.API/Migrations/20240411070633_UpdatePetGenderMigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdoptPet.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePetGenderMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "PetGender",
+                table: "Pets",
+                type: "tinyint",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "PetGender",
+                table: "Pets",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "tinyint");
+        }
+    }
+}
