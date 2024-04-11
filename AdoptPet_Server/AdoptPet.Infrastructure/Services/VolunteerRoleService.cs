d5bb0482406666d432e2454cbadc658a790bbaf1
phuhuykoxl <phuhuy02003@gmail.com>
2024-04-11 08:12:55 +0700

    add volunteerRole repository and service

---
+++ AdoptPet_Server/AdoptPet.Infrastructure/Services/VolunteerRoleService.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AdoptPet.Application.Interfaces.IRepositories;
+using AdoptPet.Domain.Entities;
+
+public class VolunteerRoleService
+{
+    private readonly IVolunteerRoleRepository _repository;
+
+    public VolunteerRoleService(IVolunteerRoleRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<VolunteerRole> GetVolunteerRoleByIdAsync(int id)
+    {
+        return await _repository.GetByIdAsync(id);
+    }
+
+    public async Task<List<VolunteerRole>> GetAllVolunteerRolesAsync()
+    {
+        return (List<VolunteerRole>)await _repository.GetAllAsync();
+    }
+
+    public async Task<VolunteerRole> AddVolunteerRoleAsync(VolunteerRole role)
+    {
+        if (role == null)
+        {
+            throw new ArgumentNullException(nameof(role));
+        }
+
+        return await _repository.AddAsync(role);
+    }
+
+    public async Task<bool> UpdateVolunteerRoleAsync(VolunteerRole role)
+    {
+        if (role == null)
+        {
+            throw new ArgumentNullException(nameof(role));
+        }
+
+        var existingRole = await _repository.GetByIdAsync(role.Id);
+        if (existingRole == null)
+        {
+            throw new InvalidOperationException($"VolunteerRole with id {role.Id} not found.");
+        }
+        await _repository.UpdateAsync(role);
+        return true;
+    }
+
+    public async Task<bool> DeleteVolunteerRoleAsync(int id)
+    {
+        var existingRole = await _repository.GetByIdAsync(id);
+        if (existingRole == null)
+        {
+            throw new InvalidOperationException($"VolunteerRole with id {id} not found.");
+        }
+
+        await _repository.DeletePermanentlyAsync(existingRole);
+        return true;
+    }
+
+    public async Task<bool> SoftDeleteVolunteerRoleAsync(int id)
+    {
+        var existingRole = await _repository.GetByIdAsync(id);
+        if (existingRole == null)
+        {
+            throw new InvalidOperationException($"VolunteerRole with id {id} not found.");
+        }
+
+        await _repository.SoftDelete(existingRole);
+        return true;
+    }
+}
