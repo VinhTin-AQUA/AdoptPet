4ebd1044e027930bc2d9ff0d8d0588a63ba393b8
VinhTin-AQUA <tinhovinh@gmail.com>
2024-04-20 15:55:52 +0700

    update data  sample

---
+++ AdoptPet_Server/AdoptPet.Application/DTOs/BaseDto/UserBaseDto.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace AdoptPet.Application.DTOs.BaseDto
+{
+    public class UserBaseDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public DateTime CreateAt { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
