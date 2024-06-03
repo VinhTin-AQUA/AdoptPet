8f04f8e8b8e55ad196eedb43ae89e9f4e7c0d6ff
VinhTin-AQUA <tinhovinh@gmail.com>
2024-06-03 16:03:07 +0700

    update search

--- AdoptPet_Client/src/app/adopt-pages/adopt/adopt.component.ts
+++ AdoptPet_Client/src/app/adopt-pages/adopt/adopt.component.ts
@@ -7,33 +7,47 @@ import { BreedService } from '../../services/breed.service';
 import { Breed } from '../../shared/models/breed/breed';
 import { PetDto } from '../../shared/models/pet/PetDto';
 import { environment } from '../../../environments/environment.development';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'app-adopt',
 	standalone: true,
-	imports: [RouterLink],
+	imports: [RouterLink, ReactiveFormsModule],
 	templateUrl: './adopt.component.html',
 	styleUrl: './adopt.component.scss',
 })
 export class AdoptComponent {
 	pageNumber: number = 1;
-	pageSize: number = 5;
+	pageSize: number = 10;
 	colors: Color[] = [];
 	breeds: Breed[] = [];
 	pets: PetDto[] = [];
 	baseImg = environment.baseImgUrl;
 
+	// search
+	petSearchForm!: FormGroup;
+	isSeached: boolean = false;
 
 	constructor(
 		private petService: PetService,
 		private colourService: ColorService,
-		private breedService: BreedService
+		private breedService: BreedService,
+		private formBuilder: FormBuilder
 	) {}
 
 	ngOnInit() {
 		this.getColor();
 		this.getBreeds();
 		this.getPets();
+
+		this.petSearchForm = this.formBuilder.group({
+			name: [''],
+			breedNames: [[]],
+			colourNames: [[]],
+			gender: [0],
+			desexed: [false],
+			ageRange: [''],
+		});
 	}
 
 	private getColor() {
@@ -74,18 +88,48 @@ export class AdoptComponent {
 
 	previousPage() {
 		this.pageNumber--;
-		if(this.pageNumber <= 0) {
-			this.pageNumber = 1
+		if (this.pageNumber <= 0) {
+			this.pageNumber = 1;
 		}
-		this.getPets();
+		if(this.isSeached === false) {
+			this.getPets();
+		} else {
+			this.search()
+		}
+		
 	}
 
 	nextPage() {
 		this.pageNumber++;
-		this.getPets();
+		if(this.isSeached === false) {
+			this.getPets();
+		} else {
+			this.search()
+		}
 	}
 
-	search () {
-		
+	search() {
+		this.isSeached = true;
+		const dataSearch = {
+			name: this.petSearchForm.controls['name'].value,
+			breedNames: this.petSearchForm.controls['breedNames'].value,
+			colourNames: this.petSearchForm.controls['colourNames'].value,
+			gender: this.petSearchForm.controls['gender'].value,
+			desexed: this.petSearchForm.controls['desexed'].value === '1',
+			ageRange: this.petSearchForm.controls['ageRange'].value,
+		};
+
+		// console.log(dataSearch);
+		this.petService.search(dataSearch, this.pageNumber, this.pageSize).subscribe({
+			next: (res: any) => {
+				console.log(res.data);
+				if (res.data.length > 0) {
+					this.pets = res.data;
+				}
+			},
+			error: err => {
+				console.log(err.error);
+			},
+		});
 	}
 }
