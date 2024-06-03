c99b7b14c4f18c76c2797e0a5d190c7f124cba2c
VinhTin-AQUA <tinhovinh@gmail.com>
2024-06-03 11:12:11 +0700

    update

--- AdoptPet_Client/src/app/admin-dashboard/pet/add-pet/add-pet.component.ts
+++ AdoptPet_Client/src/app/admin-dashboard/pet/add-pet/add-pet.component.ts
@@ -9,6 +9,10 @@ import {
 	Validators,
 } from '@angular/forms';
 import { LocationService } from '../../../services/location.service';
+import { BreedService } from '../../../services/breed.service';
+import { PetService } from '../../../services/pet.service';
+import { ColorService } from '../../../services/color.service';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-add-pet',
@@ -23,75 +27,120 @@ export class AddPetComponent {
 	districts: any = [];
 	wards: any = [];
 
-	constructor(private formBuilder: FormBuilder, private locationService: LocationService) {}
+	breeds: any = [];
+	dataImages: any = [];
+	filesAdd: any = [];
+	locations: any = [];
+	colors: any = [];
+
+	constructor(
+		private formBuilder: FormBuilder,
+		private breedService: BreedService,
+		private locationService: LocationService,
+		private petService: PetService,
+		private colourService: ColorService,
+		private router: Router
+	) {}
 
 	ngOnInit() {
 		this.petForm = this.formBuilder.group({
 			petName: ['', [Validators.required]],
-			email: ['', [Validators.email, Validators.required]],
-			age: ['', [Validators.required]],
-			province: ['Thành phố Hà Nội', [Validators.required]],
-			district: ['', [Validators.required]],
-			ward: ['', [Validators.required]],
-			street: ['', [Validators.required]],
-			weight: [0, [Validators.required]],
+			petDescription: ['', [Validators.required]],
+			petWeight: [0, [Validators.required]],
+			petAge: ['', [Validators.required]],
 			petGender: [0],
 			petDesexed: [0],
 			petWormed: [0],
 			petVaccined: [0],
 			microchipped: [0],
+			breedId: [0],
 			petEntryDate: [new Date()],
+			locationId: [0],
+			colourId: [0],
 		});
 
-		this.locationService.getProvices().subscribe({
+		this.getBreeds();
+		this.getLocations();
+		this.getColours();
+	}
+
+	private getColours() {
+		this.colourService.getAllColor(1,57).subscribe({
+			next: (res:any) => {
+				// console.log(res.items);
+				this.colors = res.items;
+				
+			}
+		})
+	}
+
+	private getLocations() {
+		this.locationService.getLocations(1,20).subscribe({
+			next: (res:any) => {
+				// console.log(res.data);
+				this.locations = res.data
+			}
+
+		})
+	}
+
+	private getBreeds() {
+		this.breedService.getAllBreed(1, 25).subscribe({
 			next: (res: any) => {
-				this.provinces = res.results;
-				// console.log(this.provinces);
-				// this.petForm.controls['province'].setValue('01');
-				this.getDistricts('01');
+				// console.log(res.data);
+				this.breeds = res.data;
 			},
 		});
 	}
 
-	private getDistricts(provinceId: string) {
-		this.locationService.getDistricts(provinceId).subscribe({
-			next: (res: any) => {
-				this.districts = res.results;
-				// console.log(this.provinces);
-				this.petForm.controls['district'].setValue(this.districts[0].district_name);
-				this.getWards(this.districts[0].district_id);
-			},
-		});
-	}
+	onImageSelected(event: any) {
+		if (event.target.files.length > 0) {
+			this.filesAdd = [];
+			this.dataImages = [];
+			for (let file of event.target.files) {
+				this.filesAdd.push(file);
 
-	private getWards(districtId: string) {
-		this.locationService.getWards(districtId).subscribe({
-			next: (res: any) => {
-				this.wards = res.results;
-				this.petForm.controls['ward'].setValue(this.wards[0].ward_name);
-			},
-		});
-	}
-
-	onProviceChanged(event: any) {
-		const selectedOption = event.target.selectedOptions[0];
-		// console.log(selectedOption.getAttribute('id'));
-		this.getDistricts(selectedOption.getAttribute('id'));
-		
-	}
-
-	onDistrictChanged(event: any) {
-		const selectedOption = event.target.selectedOptions[0];
-		this.getWards(selectedOption.getAttribute('id'));
-	}
-
-	onWardsChanged(event: any) {
-		// const selectedOption = event.target.selectedOptions[0];
-		console.log('Wards');
-		// this.getWards(selectedOption.getAttribute('id'))
+				const reader = new FileReader();
+				reader.readAsDataURL(file);
+				reader.onload = event => {
+					const data = event.target?.result;
+					this.dataImages.push(data);
+					// console.log(this.dataImages);
+				};
+			}
+		}
 	}
 
 	onSubmit() {
-		console.log(this.petForm.value);
+		const form = new FormData();
+		form.append('petName', this.petForm.controls['petName'].value);
+		form.append('petDescription', this.petForm.controls['petDescription'].value);
+		form.append('petWeight', this.petForm.controls['petWeight'].value);
+		form.append('petAge', this.petForm.controls['petAge'].value);
+		form.append('petGender', this.petForm.controls['petGender'].value);
+		form.append('petDesexed', this.petForm.controls['petDesexed'].value);
+		form.append('petWormed', this.petForm.controls['petWormed'].value);
+		form.append('petVaccined', this.petForm.controls['petVaccined'].value);
+		form.append('microchipped', this.petForm.controls['microchipped'].value);
+		form.append('petEntryDate', this.petForm.controls['petEntryDate'].value);
+		form.append('locationId', this.petForm.controls['locationId'].value);
+		form.append('breedId', this.petForm.controls['breedId'].value);
+		form.append('colourId', this.petForm.controls['colourId'].value);
+
+		for(let i of this.filesAdd) {
+			form.append('Images', i);
+		}
+		// console.log(1234);
+		
+		this.petService.addPet(form).subscribe({
+			next:(res: any) => {
+				console.log(res);
+				this.router.navigateByUrl('/admin/pet-manager')
+			},
+			error: (err) => {
+				console.log(err.error);
+				
+			}
+		})
 	}
 }
--- AdoptPet_Server/AdoptPet.Domain/Entities/Pet.cs
+++ AdoptPet_Server/AdoptPet.Domain/Entities/Pet.cs
@@ -11,7 +11,7 @@ namespace AdoptPet.Domain.Entities
 
         [Required(ErrorMessage = "PetName is required")]
         public string PetName { get; set; } = string.Empty;
-        public string PetDescription { get; set; } = string.Empty;
+        public string? PetDescription { get; set; } = string.Empty;
         public decimal PetWeight { get; set; }
         public string PetAge { get; set; } = string.Empty;
         public byte PetGender { get; set; }
@@ -28,8 +28,10 @@ namespace AdoptPet.Domain.Entities
         public int? LocationId { get; set; }
         public Location? Location {  get; set; }
         public int? OwnerId { get; set; }
+        public int BreedId { get; set; }
         public Breed? breed { get; set; }
+        public int ColourId { get; set; }
         public Colour? colour { get; set; }
-        public List<PetImage>? PetImages { get; set; } = [];
+        public List<PetImage> PetImages { get; set; } = [];
     }
 }
